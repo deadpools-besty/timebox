e10c54551e615e6f909ff7f82f812948fc51f38c
Nana Andoh <nanaegyir2000@hotmail.com>
2021-01-16 22:18:20 -0700

    moved routing and rendering from init to routes.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,5 @@
-from flask import Flask, render_template, send_from_directory
-import os
-
+from flask import Flask
 app = Flask(__name__)
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'timebox-logo.png', mimetype='image/vnd.microsoft.icon')
     
 if __name__ == '__main__':
     app.run(debug=True)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
 from app import app
-from flask import render_template
+from flask import render_template, send_from_directory, Flask
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Main')
+
+    
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'timebox-logo.png', mimetype='image/vnd.microsoft.icon')
